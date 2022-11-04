e35f38bf10e319add519c01f6f122c0921835b4c
Leonardo Costa <leonardo.costa@unifesp.br>
2022-11-04 02:17:37 -0300

    feat: splashScreen

--- src/screens/SpellBooks/index.jsx
+++ src/screens/SpellBooks/index.jsx
@@ -1,11 +1,28 @@
 import { StyleSheet, Text, View } from "react-native";
+import { BigTitle, SearchBar } from "../../atom";
 import React from "react";
 import colors from "../../misc/Colors";
+
+const icons = {
+  search: require("../../../assets/search.png"),
+};
 
 function SpellBooks() {
   return (
     <View style={styles.container}>
-      <Text>SpellBooks</Text>
+      <View style={styles.topPart}>
+        <View
+          style={{
+            justifyContent: "space-between",
+            flexDirection: "row",
+            alignItems: "center",
+            marginBottom: 20,
+          }}
+        >
+          <BigTitle>SpellBooks</BigTitle>
+        </View>
+        <SearchBar icon={icons.search} />
+      </View>
     </View>
   );
 }
@@ -16,6 +33,13 @@ const styles = StyleSheet.create({
     backgroundColor: colors.white,
     justifyContent: "center",
     alignItems: "center",
+    width: "100%",
+  },
+  topPart: {
+    flex: 1,
+    paddingHorizontal: 15,
+    paddingTop: 60,
+    width: "100%",
   },
 });
 
--- src/screens/SplashScreen/index.jsx
+++ src/screens/SplashScreen/index.jsx
@@ -1,10 +1,21 @@
 import { Button, StyleSheet, Text, View } from "react-native";
+import { Icon } from "../../atom";
 import React from "react";
+import colors from "../../misc/Colors";
+
+const icons = {
+  logo: require("../../../assets/logo.png"),
+};
 
 function SplashScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>SplashScreen</Text>
+      <Icon image={icons.logo} size={160} color={colors.dark_gray} />
+      <View style={{ flexDirection: "row", marginTop: 40 }}>
+        <Text style={[styles.title]}>Spell</Text>
+        <Text style={[styles.title, { fontWeight: "900" }]}>Wizard</Text>
+      </View>
+
       <Button
         title={"Onboarding"}
         onPress={() => {
@@ -20,6 +31,12 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: colors.primary,
+  },
+  title: {
+    fontSize: 60,
+    color: colors.dark_gray,
+    fontWeight: "600",
   },
 });
 
