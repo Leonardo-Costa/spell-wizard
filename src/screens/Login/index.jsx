3b8fbd73dbb12d2bdacc1eec81acdbf3329e74c2
Leonardo Costa <leonardo.costa@unifesp.br>
2022-11-06 01:10:12 -0300

    feat: login page implementada

--- src/screens/Login/index.jsx
+++ src/screens/Login/index.jsx
@@ -1,16 +1,39 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { Icon } from "../../atom";
 import React from "react";
+import colors from "../../misc/Colors";
+
+const icons = {
+  logo: require("../../../assets/logo.png"),
+  button: require("../../../assets/Frame.png"),
+};
 
 function Login({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Login</Text>
-      <Button
-        title={"tabs"}
+      <Icon image={icons.logo} size={160} color={colors.dark_gray} />
+      <View style={{ flexDirection: "row", marginTop: 40, marginBottom: 80 }}>
+        <Text style={[styles.title]}>Spell</Text>
+        <Text style={[styles.title, { fontWeight: "900" }]}>Wizard</Text>
+      </View>
+      <TouchableOpacity style={{ marginBottom: 10, elevation: 20 }}>
+        <Image source={icons.button} />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={{ padding: 20 }}
         onPress={() => {
           navigation.navigate("Tabs");
         }}
-      />
+      >
+        <Text style={styles.button}>Skip</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -20,6 +43,18 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: colors.primary,
+  },
+  title: {
+    fontSize: 60,
+    color: colors.dark_gray,
+    fontWeight: "600",
+  },
+  button: {
+    fontSize: 18,
+    color: colors.dark_gray,
+    fontWeight: "600",
+    textDecorationLine: "underline",
   },
 });
 
